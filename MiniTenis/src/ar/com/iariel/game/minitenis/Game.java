4eb61b6298d10fce8459163b919773100df3fdef
iarielduarte <iariel.javauser@gmail.com>
2014-12-07 17:30:04 +0100

    added

---
+++ MiniTenis/src/ar/com/iariel/game/minitenis/Game.java
@@ -0,0 +1,13 @@
+package ar.com.iariel.game.minitenis;
+
+import javax.swing.JFrame;
+
+public class Game {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Mini Tennis - a.duarte©2012");
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
+
